e05a010a567cca34333bd31100f9e46bece20530
Ryan Campbell <ryan.campbell@gmail.com>
2010-09-01 15:36:32 -0500

    add dependency on util

--- src/main/java/com/cloudbees/mvntest/App.java
+++ src/main/java/com/cloudbees/mvntest/App.java
@@ -1,6 +1,7 @@
 package com.cloudbees.mvntest;
 
 import com.cloudbees.mvntest.core.Logger;
+import com.cloudbees.mvntest.util.Math;
 
 /**
  * Hello world!
@@ -11,5 +12,6 @@ public class App
     public static void main( String[] args )
     {
         Logger.log("Hello World!");
+	Logger.log("1 + 1 = " + Math.add(1,1));
     }
 }
