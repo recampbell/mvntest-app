4f65567717f100321791d2c43e9af87f18c9fd24
Ryan Campbell <ryan.campbell@gmail.com>
2010-09-01 08:37:57 -0500

    initial commit

---
+++ src/main/java/com/cloudbees/mvntest/App.java
@@ -0,0 +1,15 @@
+package com.cloudbees.mvntest;
+
+import com.cloudbees.mvntest.core.Logger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Logger.log("Hello World!");
+    }
+}
