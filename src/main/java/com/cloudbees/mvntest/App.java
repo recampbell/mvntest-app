8f9050baf53df43cd8f5659cc451da94c741362f
Ryan Campbell <ryan.campbell@gmail.com>
2012-06-01 11:04:31 -0500

    call add again

--- src/main/java/com/cloudbees/mvntest/App.java
+++ src/main/java/com/cloudbees/mvntest/App.java
@@ -12,6 +12,6 @@ public class App
     public static void main( String[] args )
     {
         Logger.log("Hello World!");
-	Logger.log("1 / 1 = " + Math.divide(1,1));
+	Logger.log("1 + 1 + 1 = " + Math.add(1,1,1));
     }
 }
